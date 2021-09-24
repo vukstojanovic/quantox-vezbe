6e8d133600f3cece4e9bcad303593ba704e28998
vukstojanovic <vuks838@gmail.com>
2021-09-24 19:17:09 +0200

    it should be ok now

--- pod-request/app.js
+++ pod-request/app.js
@@ -3,6 +3,10 @@ const $email = document.getElementById('email-mobile');
 const $warning = document.getElementById('warning-mobile');
 const $form = document.getElementById('form-mobile');
 
+const $emailTablet = document.getElementById('email-tablet');
+const $warningTablet = document.getElementById('warning-tablet');
+const $formTablet = document.getElementById('form-tablet');
+
 $form.addEventListener("submit", (e) => {
 
     e.preventDefault();
@@ -19,7 +23,20 @@ $form.addEventListener("submit", (e) => {
 });
 
 
+$formTablet.addEventListener("submit", (e) => {
 
+    e.preventDefault();
+
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (!$emailTablet.value) {
+        $warningTablet.innerHTML = "Oops! Please add your email";
+    } else if (!$emailTablet.value.match(pattern)) {
+        $warningTablet.innerHTML = "Oops! Please check your email";
+    } else {
+        $warningTablet.innerHTML = "&nbsp;";
+    }
+});
 
 
 
