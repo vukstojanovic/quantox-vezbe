230566805718388f36ddffa1d4b0380f500caf87
vukstojanovic <vuks838@gmail.com>
2021-09-27 10:51:48 +0200

    final commit before checking

--- pod-request/app.js
+++ pod-request/app.js
@@ -7,11 +7,7 @@ const $emailTablet = document.getElementById('email-tablet');
 const $warningTablet = document.getElementById('warning-tablet');
 const $formTablet = document.getElementById('form-tablet');
 
-<<<<<<< HEAD
 $formMobile.addEventListener("submit", (e) => {
-=======
-$form.addEventListener("submit", (e) => {
->>>>>>> a5e337aa1c781fc3aaa3cd3c3270e31316dab02b
 
     e.preventDefault();
 
@@ -24,7 +20,10 @@ $form.addEventListener("submit", (e) => {
     } else {
         $warningMobile.innerHTML = "&nbsp;";
     }
+
 });
+
+
 
 
 $formTablet.addEventListener("submit", (e) => {
@@ -32,40 +31,17 @@ $formTablet.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-<<<<<<< HEAD
 
     if (!$emailTablet.value) {
         $warningTablet.innerHTML = "Oops! Please add your email";
     } else if (!$emailTablet.value.match(pattern)) {
         $warningTablet.innerHTML = "Oops! Please check your email";
     } else {
-        $warningTablet.innerHTML = "&nbsp;";
-    }
-});
-
-document.body.addEventListener('click', (e) => {
-    console.log(e.target);
-    if (!(e.target === $emailTablet)) {
         $warningTablet.innerHTML = "&nbsp;";
     }
     
 });
 
-
-
-
-
-=======
->>>>>>> a5e337aa1c781fc3aaa3cd3c3270e31316dab02b
-
-    if (!$emailTablet.value) {
-        $warningTablet.innerHTML = "Oops! Please add your email";
-    } else if (!$emailTablet.value.match(pattern)) {
-        $warningTablet.innerHTML = "Oops! Please check your email";
-    } else {
-        $warningTablet.innerHTML = "&nbsp;";
-    }
-});
 
 
 
