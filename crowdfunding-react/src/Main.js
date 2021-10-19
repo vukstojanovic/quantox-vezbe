ec470fea43fb91cf49837a8ebae0d65ddbc37062
vukstojanovic <vuks838@gmail.com>
2021-10-19 16:56:01 +0200

    classnames added

--- crowdfunding-react/src/Main.js
+++ crowdfunding-react/src/Main.js
@@ -31,39 +31,39 @@ export default function Main() {
     return (
     <main>
     
-        <section class="first">
-          <div class="upper-img">
+        <section className="first">
+          <div className="upper-img">
             <img src={logoMastercraft} alt="logoMastercraft"/>
           </div>
           <h1>Mastercraft Bamboo Monitor Riser</h1>
           <p>A beautiful a handcrafted monitor stand to reduce neck and eye strain.</p>
-          <div class="buttons">
-            <button class="back" onClick={() => setShowModal(true)}>Back this project</button>
-            <div class={isBookmarked ?"bookmark bookmark-active" : "bookmark"} onClick={() => setIsBookmarked(prev => !prev)}><img src={iconBookmark} alt="bookmark"/><span>Bookmark</span></div>
+          <div className="buttons">
+            <button className="back" onClick={() => setShowModal(true)}>Back this project</button>
+            <div className={isBookmarked ?"bookmark bookmark-active" : "bookmark"} onClick={() => setIsBookmarked(prev => !prev)}><img src={iconBookmark} alt="bookmark"/><span>Bookmark</span></div>
           </div>
         </section>
     
-        <section class="second">
+        <section className="second">
           <article>
-            <p class="nums">${numberWithCommas(totalMoney)}</p>
-            <p class="description">of $100,000 backed</p>
+            <p className="nums">${numberWithCommas(totalMoney)}</p>
+            <p className="description">of $100,000 backed</p>
           </article>
           <hr/>
           <article>
-            <p class="nums">{numberWithCommas(totalBackers)}</p>
-            <p class="description">total backers</p>
+            <p className="nums">{numberWithCommas(totalBackers)}</p>
+            <p className="description">total backers</p>
           </article>
           <hr/>
           <article>
-            <p class="nums">56</p>
-            <p class="description">days left</p>
+            <p className="nums">56</p>
+            <p className="description">days left</p>
           </article>
-          <div class="progress-bar">
-            <div class="progress" ref={bar}></div>
+          <div className="progress-bar">
+            <div className="progress" ref={bar}></div>
           </div>
         </section>
     
-        <section class="third">
+        <section className="third">
           <h2>About this project</h2>
           <p>
             The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen 
@@ -74,49 +74,49 @@ export default function Main() {
             to allow notepads, pens, and USB sticks to be stored under the stand.
           </p>
     
-          <aside className="product">
-            <h3 class="black">Bamboo Stand</h3>
-            <h3 class="green">Pledge $25 or more</h3>
+          <aside classNameName="product">
+            <h3 className="black">Bamboo Stand</h3>
+            <h3 className="green">Pledge $25 or more</h3>
             <p>
               You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and 
               you’ll be added to a special Backer member list.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{bambooStand}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{bambooStand}</span>
+              <span className="small-letter">left</span>
             </div>
             <button onClick={() => setShowModal(true)}>{bambooStand ? "Select Reward" : "Out of stock"}</button>
-            <div className={bambooStand ? "none" : "unavailable-item"}></div>
+            <div classNameName={bambooStand ? "none" : "unavailable-item"}></div>
           </aside>
     
-          <aside className="product">
-            <h3 class="black">Black Edition Stand</h3>
-            <h3 class="green">Pledge $75 or more</h3>
+          <aside classNameName="product">
+            <h3 className="black">Black Edition Stand</h3>
+            <h3 className="green">Pledge $75 or more</h3>
             <p>
               You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer 
               member list. Shipping is included.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{blackEditionStand}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{blackEditionStand}</span>
+              <span className="small-letter">left</span>
             </div>
             <button onClick={() => setShowModal(true)}>{blackEditionStand ? "Select Reward" : "Out of stock"}</button>
-            <div className={blackEditionStand ? "none" : "unavailable-item"}></div>
+            <div classNameName={blackEditionStand ? "none" : "unavailable-item"}></div>
           </aside>
     
-          <aside className="product">
-            <h3 class="black">Mahogany Special Edition</h3>
-            <h3 class="green">Pledge $200 or more</h3>
+          <aside classNameName="product">
+            <h3 className="black">Mahogany Special Edition</h3>
+            <h3 className="green">Pledge $200 or more</h3>
             <p>
               You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added 
               to our Backer member list. Shipping is included.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{mahagonySpecialEdition}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{mahagonySpecialEdition}</span>
+              <span className="small-letter">left</span>
             </div>
             <button onClick={() => setShowModal(true)}>{mahagonySpecialEdition ? "Select Reward" : "Out of stock"}</button>
-            <div className={mahagonySpecialEdition ? "none" : "unavailable-item"}></div>
+            <div classNameName={mahagonySpecialEdition ? "none" : "unavailable-item"}></div>
           </aside>
         </section>
       </main>
--- crowdfunding-react/src/Modal.js
+++ crowdfunding-react/src/Modal.js
@@ -52,29 +52,29 @@ export default function Modal() {
 
     return(
     <div className={showModal ? "modal-background" : "modal-background js-hidden"}>
-        <div class="modal">
+        <div className="modal">
           <img src={closeModal} alt="closeModal" className="closeModal" onClick={() => setShowModal(false)}/>
           <h2>Back this project</h2>
           <p>
             Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?
           </p>
   
-          <aside class="product">
-            <div class="radio-container no-reward">
-                <label for="myRadioId" class="radio">
+          <aside className="product">
+            <div className="radio-container no-reward">
+                <label for="myRadioId" className="radio">
                   <input type="radio" name="radioName" id="no-reward" onClick={displayLower}/>
-                  <div class="radio-colored"></div>
+                  <div className="radio-colored"></div>
                 </label>
-                <div class="heading-container">
-                    <h3 class="black">Pledge with no reward</h3>
+                <div className="heading-container">
+                    <h3 className="black">Pledge with no reward</h3>
                 </div>
             </div>
             <p>
               Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.
             </p>
-            <div class="lower js-hidden">
-                <p class="lower-text">Enter your pledge</p>
-                <div class="lower-inputs">
+            <div className="lower js-hidden">
+                <p className="lower-text">Enter your pledge</p>
+                <div className="lower-inputs">
                     <input type="number" placeholder="$" value={currentPledge} onChange={(e) => setCurrentPledge(e.target.value)}/>
                     <button onClick={pledgeMoney}>Continue</button>
                 </div>
@@ -82,27 +82,27 @@ export default function Modal() {
           </aside>
       
           <aside className="product">
-            <div class="radio-container">
-                <label for="myRadioId" class="radio">
+            <div className="radio-container">
+                <label for="myRadioId" className="radio">
                   <input type="radio" name="radioName" id="bambooStand" onClick={displayLower}/>
-                  <div class="radio-colored"></div>
+                  <div className="radio-colored"></div>
                 </label>
-                <div class="heading-container">
-                    <h3 class="black">Bamboo Stand</h3>
-                    <h3 class="green">Pledge $25 or more</h3>
+                <div className="heading-container">
+                    <h3 className="black">Bamboo Stand</h3>
+                    <h3 className="green">Pledge $25 or more</h3>
                 </div>
             </div>
             <p>
               You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and 
               you’ll be added to a special Backer member list.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{bambooStand}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{bambooStand}</span>
+              <span className="small-letter">left</span>
             </div>
-            <div class="lower js-hidden">
-                <p class="lower-text">Enter your pledge</p>
-                <div class="lower-inputs">
+            <div className="lower js-hidden">
+                <p className="lower-text">Enter your pledge</p>
+                <div className="lower-inputs">
                     <input type="number" placeholder="$" min={25} value={currentPledge} onChange={(e) => setCurrentPledge(e.target.value)}/>
                     <button onClick={pledgeMoney}>Continue</button>
                 </div>
@@ -111,27 +111,27 @@ export default function Modal() {
           </aside>
       
           <aside className="product">
-            <div class="radio-container">
-                <label for="myRadioId" class="radio">
+            <div className="radio-container">
+                <label className="radio">
                   <input type="radio" name="radioName" id="blackEditionStand" onClick={displayLower}/>
-                  <div class="radio-colored"></div>
+                  <div className="radio-colored"></div>
                 </label>
-                <div class="heading-container">
-                    <h3 class="black">Black Edition Stand</h3>
-                    <h3 class="green">Pledge $75 or more</h3>
+                <div className="heading-container">
+                    <h3 className="black">Black Edition Stand</h3>
+                    <h3 className="green">Pledge $75 or more</h3>
                 </div>
             </div>
             <p>
               You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer 
               member list. Shipping is included.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{blackEditionStand}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{blackEditionStand}</span>
+              <span className="small-letter">left</span>
             </div>
-            <div class="lower js-hidden">
-                <p class="lower-text">Enter your pledge</p>
-                <div class="lower-inputs">
+            <div className="lower js-hidden">
+                <p className="lower-text">Enter your pledge</p>
+                <div className="lower-inputs">
                     <input type="number" placeholder="$" min={75} value={currentPledge} onChange={(e) => setCurrentPledge(e.target.value)}/>
                     <button onClick={pledgeMoney}>Continue</button>
                 </div>
@@ -140,27 +140,27 @@ export default function Modal() {
           </aside>
       
           <aside className="product">
-            <div class="radio-container">
-                <label for="myRadioId" class="radio">
+            <div className="radio-container">
+                <label for="myRadioId" className="radio">
                   <input type="radio" name="radioName" id="mahoganyStand" onClick={displayLower}/>
-                  <div class="radio-colored"></div>
+                  <div className="radio-colored"></div>
                 </label>
-                <div class="heading-container">
-                    <h3 class="black">Mahogany Special Edition</h3>
-                    <h3 class="green">Pledge $200 or more</h3>
+                <div className="heading-container">
+                    <h3 className="black">Mahogany Special Edition</h3>
+                    <h3 className="green">Pledge $200 or more</h3>
                 </div>
             </div>
             <p>
               You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added 
               to our Backer member list. Shipping is included.
             </p>
-            <div class="how-many">
-              <span class="big-letter">{mahagonySpecialEdition}</span>
-              <span class="small-letter">left</span>
+            <div className="how-many">
+              <span className="big-letter">{mahagonySpecialEdition}</span>
+              <span className="small-letter">left</span>
             </div>
-            <div class="lower js-hidden">
-                <p class="lower-text">Enter your pledge</p>
-                <div class="lower-inputs">
+            <div className="lower js-hidden">
+                <p className="lower-text">Enter your pledge</p>
+                <div className="lower-inputs">
                     <input type="number" placeholder="$" min={200} value={currentPledge} onChange={(e) => setCurrentPledge(e.target.value)}/>
                     <button onClick={pledgeMoney}>Continue</button>
                 </div>
