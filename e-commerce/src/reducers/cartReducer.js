ef8d3245a08de8ad25d0829cda056a1b7609054b
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-11-15 17:59:31 +0100

    Remove breaks

--- e-commerce/src/reducers/cartReducer.js
+++ e-commerce/src/reducers/cartReducer.js
@@ -16,19 +16,14 @@ function cartReducer(state = initState(), action) {
                 return state;
             }
             return [...state, action.payload];
-            break;
         case "INCREASE":
             return state.map(item => item.id === action.payload ? {...item, amount: item.amount + 1} : item);
-            break;
         case "DECREASE":
             return state.map(item => item.id === action.payload ? {...item, amount: item.amount > 1 ? item.amount - 1 : 1} : item);
-            break;
         case "REMOVE":
             return state.filter(item => item.id !== action.payload);
-            break;
         case "EMPTY":
             return [];
-            break;
         default:
             return state;
     }
