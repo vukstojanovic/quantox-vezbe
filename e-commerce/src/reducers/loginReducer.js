560c0e3e3227bfadc86fcd6c16a09c933e026070
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-11-23 20:13:28 +0100

    Remove case logout

--- e-commerce/src/reducers/loginReducer.js
+++ e-commerce/src/reducers/loginReducer.js
@@ -13,8 +13,6 @@ function loginReducer(state = initState(), action) {
     switch (action.type) {
         case "LOGIN":
             return true;
-        case "LOGOUT":
-            return false;
         default:
             return state;
     }
