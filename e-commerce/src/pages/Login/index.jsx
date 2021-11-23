15a167e031a557c5cb7e30ef8675d31573822a0f
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-11-23 20:35:33 +0100

    Change navigate dispatch order

--- e-commerce/src/pages/Login/index.jsx
+++ e-commerce/src/pages/Login/index.jsx
@@ -29,8 +29,8 @@ function Login() {
                     localStorage.setItem("accessToken", response.data.accessToken);
                     localStorage.setItem("refreshToken", response.data.refreshToken);
                     localStorage.setItem("currentUsername", username);
-                    navigate('/');
                     dispatch(login());
+                    navigate('/');
                 } else {
                     alert("Wrong username or password!");
                 }
