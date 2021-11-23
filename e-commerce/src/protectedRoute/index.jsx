fd1523a72fa7618c792e0d4647114ec8be035974
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-11-23 13:36:45 +0100

    Add clear

--- e-commerce/src/protectedRoute/index.jsx
+++ e-commerce/src/protectedRoute/index.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 
 function ProtectedRoute({children}) {
     const isLogged = useSelector(state => state.loginReducer);
+    console.log(isLogged);
     return (
         isLogged ? children : <Navigate to="/login" />
     )
