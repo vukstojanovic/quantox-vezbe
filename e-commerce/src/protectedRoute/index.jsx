a172a3e5a8694120d0228fcfaaa16022e4a9f4b5
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-11-23 20:24:05 +0100

    Fix protectedRoute

--- e-commerce/src/protectedRoute/index.jsx
+++ e-commerce/src/protectedRoute/index.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({children}) {
-    const isLogged = useSelector(state => state.cartReducer);
+    const isLogged = useSelector(state => state.loginReducer);
 
     return (
         isLogged ? children : <Navigate to="/login" />
