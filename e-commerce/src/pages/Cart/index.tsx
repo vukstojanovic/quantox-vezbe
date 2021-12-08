138bfede841bee8f5fb1382ffcbebe329a4d6ae9
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-12-08 18:04:45 +0100

    Remove React statement

--- e-commerce/src/pages/Cart/index.tsx
+++ e-commerce/src/pages/Cart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import CartItem from '../../components/CartItem/index';
 import { Link } from 'react-router-dom';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
